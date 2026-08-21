e6b574fa800058fffa136f5e9c2d5ee3d262d14e
Neil Slater <slobo777@gmail.com>
2026-08-21 22:02:26 +0100

    Adopt standardized RuboCop policy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Unreleased
+
+- Require Ruby 3.3 or newer.
+
 ## 0.0.1
 
 - Initial release.
--- lib/foo.rb
+++ lib/foo.rb
@@ -16,18 +16,18 @@ module Foo
   #   Returns a test value from the native extension.
   #   @return [Integer] the value 8093
 
-  # A three-dimensional vector whose data and operations are implemented in C.
+  # @!parse
+  #   # A three-dimensional vector whose data and operations are implemented in C.
+  #   class Vector
+  #     # Creates a vector.
+  #     # @param x [Numeric] x coordinate
+  #     # @param y [Numeric] y coordinate
+  #     # @param z [Numeric] z coordinate
+  #     # @raise [TypeError] if any coordinate cannot be converted to a number
+  #     def initialize(x, y, z); end
   #
-  # @!method initialize(x, y, z)
-  #   Creates a vector.
-  #   @param x [Numeric] x coordinate
-  #   @param y [Numeric] y coordinate
-  #   @param z [Numeric] z coordinate
-  #   @raise [TypeError] if any coordinate cannot be converted to a number
-  #
-  # @!method magnitude
-  #   Calculates the vector's Euclidean length.
-  #   @return [Float] the vector magnitude
-  class Vector # rubocop:disable Lint/EmptyClass
-  end
+  #     # Calculates the vector's Euclidean length.
+  #     # @return [Float] the vector magnitude
+  #     def magnitude; end
+  #   end
 end
